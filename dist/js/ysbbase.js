b1facb124dd98199e1a13482745dd7473aacd0de
PeterZhu <taoqianbao@gmail.com>
2016-09-01 13:43:30 +0800

    build for ysb
    build for ysb

--- dist/js/ysbbase.js
+++ dist/js/ysbbase.js
@@ -1,18 +1,18 @@
 /*!
- * Bootstrap v3.3.7 (http://getbootstrap.com)
- * Copyright 2011-2016 Twitter, Inc.
+ * uiframework v3.3.7 (#)
+ * Copyright 2011-2016 YSB, Inc.
  * Licensed under the MIT license
  */
 
 if (typeof jQuery === 'undefined') {
-  throw new Error('Bootstrap\'s JavaScript requires jQuery')
+  throw new Error('uiframework\'s JavaScript requires jQuery')
 }
 
 +function ($) {
   'use strict';
   var version = $.fn.jquery.split(' ')[0].split('.')
   if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] > 3)) {
-    throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
+    throw new Error('uiframework\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
   }
 }(jQuery);
 
